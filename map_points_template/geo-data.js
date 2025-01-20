a09b14ea231c32c393197e1e07b3fdc40ef01084
shuoshuobar <yw4359@columbia.edu>
2025-01-20 14:36:15 -0500

    Create geo-data.js

---
+++ map_points_template/geo-data.js
@@ -0,0 +1,71 @@
+infoData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties":{ "name": "here", "group_name": "favs", "group_id": 1, "headline": "home", "article": "<p>some text<\/p>", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.96416664123535,
+          40.78950978441437
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "place", "group_name": "bests", "group_id": 2, "headline": "special place", "article": "<p> this is my favorite place in the park!<\/p>", "color": "#666600", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.96631240844727,
+          40.78827508312983
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "here", "group_name": "favs", "group_id": 1, "headline": "home", "article": "<p>some text<\/p>", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.96734237670898,
+          40.78580561168767
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "here", "group_name": "favs", "group_id": 1, "headline": "Center of the reservoir", "article":  "<p> don't swim here<\/p>", "color": "#000000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.96184921264648,
+          40.78645548149673
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "here", "group_name": "favs", "group_id": 1, "headline": "home", "article": "article1", "color": "#666600", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.96159172058104,
+          40.78216622323578
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "here", "group_name": "favs", "group_id": 1, "headline": "lots of jogging", "article": "<p class=\"story-body-text story-content\" data-para-count=\"107\" data-total-count=\"145\"><b>A WHOLE ARTICLE!</B><BR><BR>Last week, this native New Yorker completed a rite of passage of sorts: riding the <a href=\"https://www.nytimes.com/2017/06/23/nyregion/coney-island-cyclone-roller-coaster-richard-rodriguez.html\">Cyclone</a> at Coney Island.</p><p class=\"story-body-text story-content\" data-para-count=\"77\" data-total-count=\"222\">The roller coaster turned 90 today, and it was time I finally experienced it.</p><p class=\"story-body-text story-content\" data-para-count=\"213\" data-total-count=\"435\">Why hadn\u2019t I done it before? A quick text to my mom gave me some clarity. \u201cYou lived a sheltered childhood?!\u201d she replied. I have a feeling it was more about schlepping a bouncy kid on a lengthy subway ride.</p><p class=\"story-body-text story-content\" data-para-count=\"226\" data-total-count=\"661\">I had imagined the Cyclone as a slow ride up and down a creaky wooden track \u2014 all 2,640 feet of it. Instead, I was both pleasantly surprised and slightly frustrated that I hadn\u2019t done this sooner, like other New York kids.</p><p class=\"story-body-text story-content\" data-para-count=\"122\" data-total-count=\"783\">\u201cWe grew up on it,\u201d said Vida Williams, 55, a Coney Islander. \u201cIf they changed it, I probably wouldn\u2019t ride it.\u201d</p> <p class=\"story-body-text story-content\" data-para-count=\"107\" data-total-count=\"145\">Last week, this native New Yorker completed a rite of passage of sorts: riding the <a href=\"https://www.nytimes.com/2017/06/23/nyregion/coney-island-cyclone-roller-coaster-richard-rodriguez.html\">Cyclone</a> at Coney Island.</p><p class=\"story-body-text story-content\" data-para-count=\"77\" data-total-count=\"222\">The roller coaster turned 90 today, and it was time I finally experienced it.</p><p class=\"story-body-text story-content\" data-para-count=\"213\" data-total-count=\"435\">Why hadn\u2019t I done it before? A quick text to my mom gave me some clarity. \u201cYou lived a sheltered childhood?!\u201d she replied. I have a feeling it was more about schlepping a bouncy kid on a lengthy subway ride.</p><p class=\"story-body-text story-content\" data-para-count=\"226\" data-total-count=\"661\">I had imagined the Cyclone as a slow ride up and down a creaky wooden track \u2014 all 2,640 feet of it. Instead, I was both pleasantly surprised and slightly frustrated that I hadn\u2019t done this sooner, like other New York kids.</p><p class=\"story-body-text story-content\" data-para-count=\"122\" data-total-count=\"783\">\u201cWe grew up on it,\u201d said Vida Williams, 55, a Coney Islander. \u201cIf they changed it, I probably wouldn\u2019t ride it.\u201d</p> <p class=\"story-body-text story-content\" data-para-count=\"107\" data-total-count=\"145\">Last week, this native New Yorker completed a rite of passage of sorts: riding the <a href=\"https://www.nytimes.com/2017/06/23/nyregion/coney-island-cyclone-roller-coaster-richard-rodriguez.html\">Cyclone</a> at Coney Island.</p><p class=\"story-body-text story-content\" data-para-count=\"77\" data-total-count=\"222\">The roller coaster turned 90 today, and it was time I finally experienced it.</p><p class=\"story-body-text story-content\" data-para-count=\"213\" data-total-count=\"435\">Why hadn\u2019t I done it before? A quick text to my mom gave me some clarity. \u201cYou lived a sheltered childhood?!\u201d she replied. I have a feeling it was more about schlepping a bouncy kid on a lengthy subway ride.</p><p class=\"story-body-text story-content\" data-para-count=\"226\" data-total-count=\"661\">I had imagined the Cyclone as a slow ride up and down a creaky wooden track \u2014 all 2,640 feet of it. Instead, I was both pleasantly surprised and slightly frustrated that I hadn\u2019t done this sooner, like other New York kids.</p><p class=\"story-body-text story-content\" data-para-count=\"122\" data-total-count=\"783\">\u201cWe grew up on it,\u201d said Vida Williams, 55, a Coney Islander. \u201cIf they changed it, I probably wouldn\u2019t ride it.\u201d</p> ", "color": "#660000", "radius": "12" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.95764350891112,
+          40.78808012819681
+        ]
+      }
+    }
+  ]
+}
